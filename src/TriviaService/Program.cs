44f7264a12d03fd8fc135c5854280c5708da0032
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2024-03-11 15:25:59 -0700

    Refactor worker into proper Windows service

--- src/TriviaService/Program.cs
+++ src/TriviaService/Program.cs
@@ -1,7 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+
 using TriviaService;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "Trivia Service";
+});
+
+if (OperatingSystem.IsWindows())
+{
+    LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(builder.Services);
+}
+
+builder.Services.AddHostedService<WindowsBackgroundService>();
 
 var host = builder.Build();
 host.Run();
